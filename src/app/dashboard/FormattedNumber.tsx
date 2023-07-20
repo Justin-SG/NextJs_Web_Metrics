0dcae9df798a26d38ae91dfbb3c6b5427beb3394
Justin-SG <schoenberg.justin@gmail.com>
2023-07-20 23:18:55 +0200

    Fixed hydration error

--- src/app/dashboard/FormattedNumber.tsx
+++ src/app/dashboard/FormattedNumber.tsx
@@ -5,10 +5,7 @@ interface Props {
 export default function FormattedNumber({ number }: Props) {
   return (
     <span>
-      {`${number?.toLocaleString(undefined, {
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2,
-      })} €` || ""}
+      {`${number} €` || ""}
     </span>
   );
 }
