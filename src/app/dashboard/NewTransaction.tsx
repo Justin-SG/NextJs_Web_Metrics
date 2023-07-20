cf92c38982f481ffafa7b498c064d1e54d9e6212
Justin-SG <schoenberg.justin@gmail.com>
2023-07-20 15:24:58 +0200

    Fixed spelling errors in attributes

--- src/app/dashboard/NewTransaction.tsx
+++ src/app/dashboard/NewTransaction.tsx
@@ -38,13 +38,13 @@ export default function NewTransaction() {
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
-                    stroke-width="1.5"
+                    strokeWidth="1.5"
                     stroke="currentColor"
                     className="h-7 w-7"
                 >
                     <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                         d="M12 4.5v15m7.5-7.5h-15"
                     />
                 </svg>
