ad263851286564a38c530e854001874feed5527e
Justin Schönberg <jschoenberg.itsb-b2020@fh-salzburg.ac.at>
2023-07-22 11:11:13 +0200

    Removed image from aws

--- src/app/landing-page/RefferalsComponent.tsx
+++ src/app/landing-page/RefferalsComponent.tsx
@@ -21,11 +21,6 @@ export default function RefferalsComponent() {
 						</p>
 					</blockquote>
 					<figcaption className="flex items-center justify-center mt-6 space-x-3">
-						<img
-							className="w-6 h-6 rounded-full"
-							src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png"
-							alt="profile"
-						/>
 						<div className="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
 							<div className="pr-3 font-medium text-gray-900 dark:text-white">Micheal Gough</div>
 							<div className="pl-3 text-sm font-light text-gray-500 dark:text-gray-400">CEO at Google</div>
